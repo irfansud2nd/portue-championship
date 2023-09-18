dd58e454f32ff73b0c0bcabbef3132e523c9978e
irfansud2nd <irfansud2nd@gmail.com>
2023-09-19 00:17:57 +0700

    fix text pop up

--- app/page.tsx
+++ app/page.tsx
@@ -23,16 +23,27 @@ export default function Home() {
       <Head>
         <title>Portue Silat Bandung Championship</title>
       </Head>
-      <Rodal visible={rodalVisible} onClose={() => setRodalVisible(false)}>
-        <div className="w-full h-full flex flex-col justify-between items-center text-justify">
-          <h1 className="text-xl font-semibold">INFO PENTING</h1>
+      <Rodal
+        visible={rodalVisible}
+        onClose={() => setRodalVisible(false)}
+        customStyles={{ height: "fit-content" }}
+      >
+        <div className="w-full h-full flex flex-col justify-between items-center text-justify mt-4">
+          <h1 className="text-xl font-semibold text-center">
+            Info PORTUE Bandung Championship 2023 Cabor Pencak Silat.
+          </h1>
           <p>
-            Sesuai dengan keputusan KONI Kota Bandung PORTUE Bandung
-            Championship 2023, Cabang Olahraga Pencak Silat. Yang semula
-            diadakan tanggal <strong>25-29 Oktober 2023</strong>, dirubah
-            menjadi tanggal <strong>23-26 Oktober 2023</strong>.
+            Sesuai dengan hasil rapat PB PORTUE Bandung Championship 2023, Pada
+            tanggal 18 September 2023 bertempat di Sekertariat PB PORTUE.
+            Terkait dengan Pembagian Venue Laga Tangkas dengan CABOR lainnya.
+            Maka dengan ini kami sampaikan bahwa jadwal CABOR Pencak Silat yang
+            semula dilaksanakan tanggal <strong>25-29 Oktober 2023</strong>,
+            dimajukan menjadi tanggal <strong>23-26 Oktober 2023</strong>.
+            <br />
+            Untuk itu kami sampaikan permohonan maaf dan atas perhatiannya kami
+            ucapkan terima kasih.
           </p>
-          <p className="font-semibold">Mohon maaf atas ketidaknyamanannya.</p>
+          <p className="font-semibold">-Panpel-</p>
           <button
             onClick={() => setRodalVisible(false)}
             className="btn_green btn_full"
