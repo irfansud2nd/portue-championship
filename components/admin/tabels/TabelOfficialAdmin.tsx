b92e1e4a013593bb5523feb099ba8b46a45dab00
irfansud2nd <irfansud2nd@gmail.com>
2023-10-20 20:54:20 +0700

    add delete to official admin

--- components/admin/tabels/TabelOfficialAdmin.tsx
+++ components/admin/tabels/TabelOfficialAdmin.tsx
@@ -8,6 +8,9 @@ import { useDownloadExcel } from "react-export-table-to-excel";
 import Rodal from "rodal";
 import "rodal/lib/rodal.css";
 import InlineLoading from "../InlineLoading";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { compare, deletePerson } from "@/utils/sharedFunctions";
 
 const TabelOfficialAdmin = () => {
   const {
@@ -30,10 +33,15 @@ const TabelOfficialAdmin = () => {
     "Email Pendaftar",
     "Waktu Pendaftaran",
     "Waktu Perubahan",
+    "Delete",
   ];
 
   const [officialsToMap, setOfficialsToMap] = useState<DataOfficialState[]>([]);
   const [showRodal, setShowRodal] = useState(false);
+  const [deleteRodal, setDeleteRodal] = useState(false);
+  const [dataToDelete, setDataToDelete] = useState<DataOfficialState | null>(
+    null
+  );
   const [fotoUrl, setFotoUrl] = useState("");
 
   const tabelRef = useRef(null);
@@ -53,8 +61,44 @@ const TabelOfficialAdmin = () => {
     }
   }, [selectedOfficials]);
 
+  const toastId = useRef(null);
+
+  // DELETE - STEP 1 - DELETE BUTTON
+  const handleDelete = (data: DataOfficialState) => {
+    setDeleteRodal(true);
+    setDataToDelete(data);
+  };
+
+  // DELETE - STEP 2 - DELETE PERSON
+  const deleteData = () => {
+    setDeleteRodal(false);
+    if (dataToDelete) {
+      deletePerson(
+        "officials",
+        dataToDelete,
+        kontingens,
+        toastId,
+        afterDeletePerson
+      );
+    }
+  };
+
+  // DELETE - STEP 3 - CALLBACK
+  const afterDeletePerson = () => {
+    cancelDelete();
+    refreshOfficials();
+  };
+
+  // DELETE CANCELER
+  const cancelDelete = () => {
+    setDeleteRodal(false);
+    setDataToDelete(null);
+  };
+
   return (
     <div>
+      <ToastContainer />
+
       <h1 className="capitalize mb-1 text-3xl font-bold border-b-2 border-black w-fit">
         Tabel Official
       </h1>
@@ -97,6 +141,33 @@ const TabelOfficialAdmin = () => {
       </Rodal>
       {/* RODAL */}
 
+      {/* DELETE RODAL */}
+      <Rodal visible={deleteRodal} onClose={() => setDeleteRodal(false)}>
+        <div className="h-full w-full">
+          <div className="h-full w-full flex flex-col justify-between">
+            <h1 className="font-semibold text-red-500">Hapus Official</h1>
+            <p>Apakah anda yakin akan menghapus Official ini?</p>
+            <div className="self-end flex gap-2">
+              <button
+                className="btn_red btn_full"
+                onClick={deleteData}
+                type="button"
+              >
+                Yakin
+              </button>
+              <button
+                className="btn_green btn_full"
+                onClick={cancelDelete}
+                type="button"
+              >
+                Batal
+              </button>
+            </div>
+          </div>
+        </div>
+      </Rodal>
+      {/* DELETE RODAL */}
+
       <table className="w-full" ref={tabelRef}>
         <thead>
           <tr>
@@ -106,39 +177,49 @@ const TabelOfficialAdmin = () => {
           </tr>
         </thead>
         <tbody>
-          {officialsToMap.map((official: DataOfficialState, i: number) => (
-            <tr key={official.id} className="border_td">
-              <td>{i + 1}</td>
-              <td>{official.id}</td>
-              <td>{official.namaLengkap.toUpperCase()}</td>
-              <td className="capitalize">
-                {findNamaKontingen(
-                  kontingens,
-                  official.idKontingen
-                ).toUpperCase()}
-              </td>
-              <td>{official.jenisKelamin}</td>
-              <td>{official.jabatan}</td>
-              <td>
-                {official.downloadFotoUrl ? (
+          {officialsToMap
+            .sort(compare("namaLengkap", "asc"))
+            .map((official: DataOfficialState, i: number) => (
+              <tr key={official.id} className="border_td">
+                <td>{i + 1}</td>
+                <td>{official.id}</td>
+                <td>{official.namaLengkap.toUpperCase()}</td>
+                <td className="capitalize">
+                  {findNamaKontingen(
+                    kontingens,
+                    official.idKontingen
+                  ).toUpperCase()}
+                </td>
+                <td>{official.jenisKelamin}</td>
+                <td>{official.jabatan}</td>
+                <td>
+                  {official.downloadFotoUrl ? (
+                    <button
+                      className="hover:text-green-500 hover:underline transition"
+                      onClick={() => {
+                        setShowRodal(true);
+                        setFotoUrl(official.downloadFotoUrl);
+                      }}
+                    >
+                      Show
+                    </button>
+                  ) : (
+                    "-"
+                  )}
+                </td>
+                <td>{official.creatorEmail}</td>
+                <td>{formatTanggal(official.waktuPendaftaran)}</td>
+                <td>{formatTanggal(official.waktuPerubahan)}</td>
+                <td>
                   <button
-                    className="hover:text-green-500 hover:underline transition"
-                    onClick={() => {
-                      setShowRodal(true);
-                      setFotoUrl(official.downloadFotoUrl);
-                    }}
+                    className="btn_red btn_full"
+                    onClick={() => handleDelete(official)}
                   >
-                    Show
+                    Delete
                   </button>
-                ) : (
-                  "-"
-                )}
-              </td>
-              <td>{official.creatorEmail}</td>
-              <td>{formatTanggal(official.waktuPendaftaran)}</td>
-              <td>{formatTanggal(official.waktuPerubahan)}</td>
-            </tr>
-          ))}
+                </td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
